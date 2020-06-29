9a2005d7ddf1c4e6f263eb7516fed97f4a5a6fd7
kantani <CarlosForever@protonmail.com>
2020-06-29 02:11:42 +0200

    i love slock

---
+++ config.h
@@ -0,0 +1,13 @@
+/* user and group to drop privileges to */
+static const char *user  = "user";
+static const char *group = "users";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "black",     /* after initialization */
+	[INPUT] =  "#005577",   /* during input */
+	[FAILED] = "#CC3333",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+static const int timetocancel = 2;
